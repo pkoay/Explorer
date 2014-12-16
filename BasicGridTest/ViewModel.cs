bd8e532ae2d9257252e8548319e29586aa363859
pkoay <pkoay@walzconstruction.com>
2014-12-17 09:16:47 +1000

    BasicGridTest

---
+++ BasicGridTest/ViewModel.cs
@@ -0,0 +1,28 @@
+﻿using BasicGridTest;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WalzExplorer.Database
+{
+    public class ViewModel
+    {
+        private readonly ObservableCollection<tblMain> mainView;
+        public readonly BASICGRIDDATAEntities context;
+
+        public ViewModel()
+        {
+            this.context = new BASICGRIDDATAEntities();
+            this.mainView = new ObservableCollection<tblMain>(context.tblMains);
+        }
+
+        public ObservableCollection<tblMain> TenderContractorsView
+        {
+            get { return this.mainView; }
+        }
+
+    }
+}
