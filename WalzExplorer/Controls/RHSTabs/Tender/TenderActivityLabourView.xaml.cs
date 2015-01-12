ce320191d692c1b52c8b1ba8124bd273b87505fe
pkoay <pkoay@walzconstruction.com>
2015-01-12 12:32:18 +1000

    Added Activity labour

---
+++ WalzExplorer/Controls/RHSTabs/Tender/TenderActivityLabourView.xaml.cs
@@ -0,0 +1,61 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using Telerik.Windows.Controls;
+using Telerik.Windows.Controls.GridView;
+using WalzExplorer.Common;
+namespace WalzExplorer.Controls.RHSTabs.Tender
+{
+    /// <summary>
+    /// Interaction logic for TenderViewer.xaml
+    /// </summary>
+    
+     
+    public partial class TenderActivityLabourView : RHSTabGridViewBase
+    {
+      
+        TenderActivityLabourViewModel vm;
+
+        public TenderActivityLabourView()
+        {
+            InitializeComponent();
+            base.SetGrid(grd);
+
+            columnNotRequired.Add("RowVersion");
+            columnNotRequired.Add("TenderID");
+            columnNotRequired.Add("ActivityID");
+            columnNotRequired.Add("ActivityLabourID");
+
+            columnNotRequired.Add("SortOrder");
+            columnNotRequired.Add("UpdatedBy");
+            columnNotRequired.Add("UpdatedDate");
+           
+        }
+
+        public override void TabLoad()
+        {
+            gridEdit = true;
+            gridDelete = true;
+            gridAdd = true;
+            
+            // set grid data
+            vm = new TenderActivityLabourViewModel(node.TypeID, user.Person.PersonID, node.IDAsInt());
+            viewModel = vm;
+            grd.DataContext = viewModel;
+            grd.ItemsSource = viewModel.data;
+            columnCombo.Add("LabourStandardID", GridLibrary.CreateCombo("cmbLabourStandradID", "LabourStandard", vm.cmbLabourStandardList(), "LabourStandardID", "Title"));
+            columnCombo.Add("StepID", GridLibrary.CreateCombo("cmbStepID", "Step", vm.cmbStepList(), "StepID", "Title"));
+            columnCombo.Add("WorkGroupID", GridLibrary.CreateCombo("cmbWorkGroupID", "WorkGroup", vm.cmbWorkGroupList(), "WorkGroupID", "Title"));
+            columnCombo.Add("UnitOfMeasureID", GridLibrary.CreateCombo("cmbUnitOfMeasureID", "Unit Of Measure", vm.cmbUnitOfMeasureList(), "UnitOfMeasureID", "Title"));
+            base.TabLoad();
+
+        }
+
+     
+      
+    }
+
+}
---
+++ WalzExplorer/Controls/RHSTabs/Tender/TenderActivityLabourViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WalzExplorer.Database;
+
+namespace WalzExplorer.Controls.RHSTabs.Tender
+{
+    public class TenderActivityLabourViewModel : RHSTabGridViewModelBase
+    {
+
+        public TenderActivityLabourViewModel(string NodeType, string PersonID, int Id)
+        {
+            switch (NodeType)
+            {
+                case "TenderActivity":
+                    data = new ObservableCollection<ModelBase>(context.tblTender_ActivityLabour
+                        .Join(context.tblTender_Step, l => l.StepID, s => s.StepID, (l, s) => new { labour = l, step = s }) //join required so we can order by step order
+                        .OrderBy(a => a.step.SortOrder) 
+                        .Select(a => a.labour)
+                        .Where(a => a.ActivityID == Id)
+                        );
+                        
+                    columnDefault.Clear();
+                    columnDefault.Add("Activity", Id);
+                    break;
+                default: throw new NotImplementedException();
+            }
+                
+        }
+
+        public override ModelBase DefaultItem()
+        {
+            tblTender_ActivityLabour i = new tblTender_ActivityLabour();
+          
+            return i;
+        }
+        public List<object> cmbLabourStandardList()
+        {
+            return context.tblTender_LabourStandard.ToList<object>();
+        }
+        public List<object> cmbStepList()
+        {
+            return context.tblTender_Step.ToList<object>();
+        }
+        public List<object> cmbWorkGroupList()
+        {
+            return context.tblTender_WorkGroup.ToList<object>();
+        }
+        public List<object> cmbUnitOfMeasureList()
+        {
+            return context.tblTender_UnitOfMeasure.ToList<object>();
+        }
+       
+    }
+}
