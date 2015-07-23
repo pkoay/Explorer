c77be0fde622de2be07107f6f5043d8adbd7d140
pkoay <pkoay@walzconstruction.com>
2015-07-23 11:02:43 +1000

    missing stuff

---
+++ WalzExplorer/Controls/RHSTabs/Tender/OverheadDetailView.xaml.cs
@@ -0,0 +1,92 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Threading;
+using Telerik.Windows.Controls;
+using Telerik.Windows.Controls.GridView;
+using Telerik.Windows.Data;
+using WalzExplorer.Common;
+using WalzExplorer.Controls.Grid;
+
+namespace WalzExplorer.Controls.RHSTabs.Tender
+{
+    /// <summary>
+    /// Interaction logic for TenderViewer.xaml
+    /// </summary>
+
+
+    public partial class OverheadDetailView : RHSTabViewBase
+    {
+
+        OverheadDetailViewModel vm;
+
+       public OverheadDetailView()
+        {
+            InitializeComponent();
+        }
+
+        public override void TabLoad()
+        {
+            vm = new OverheadDetailViewModel(settings);
+            grd.vm = vm;
+            grd.grd.DataContext = vm;
+            grd.grd.ItemsSource = vm.data;
+
+            if (settings.user.SecurityGroups.Contains("WD_Tender"))
+                grd.SetGrid(settings, true, true, true);
+            else
+                grd.SetGrid(settings, false, false, false);
+
+            grd.columnsettings.Add("OverheadTypeID", new GridEditViewBase.columnSetting() { aggregation = GridEditViewBase.columnSetting.aggregationType.COUNT });
+            grd.columnsettings.Add("WorkGroupID", new GridEditViewBase.columnSetting() { isDeveloper = true });
+            grd.columnsettings.Add("OverheadItemID", new GridEditViewBase.columnSetting() { isDeveloper = true });
+            grd.columnsettings.Add("Title", new GridEditViewBase.columnSetting() { format=  GridEditViewBase.columnSetting.formatType.TEXT});
+            grd.columnsettings.Add("Duration", new GridEditViewBase.columnSetting() { format = GridEditViewBase.columnSetting.formatType.G });
+            grd.columnsettings.Add("Count", new GridEditViewBase.columnSetting() { format = GridEditViewBase.columnSetting.formatType.G });
+            grd.columnsettings.Add("Rate", new GridEditViewBase.columnSetting() { format = GridEditViewBase.columnSetting.formatType.G });
+            grd.columnsettings.Add("Hours", new GridEditViewBase.columnSetting()
+            {
+                format = GridEditViewBase.columnSetting.formatType.N2,
+                aggregation = GridEditViewBase.columnSetting.aggregationType.SUM,
+                isReadonly = true,
+                order = 8,
+                tooltip = "Calculated from Estimate hours (for the workgroup) + Additional hours (for the workgroup)" 
+            });
+            grd.columnsettings.Add("Total", new GridEditViewBase.columnSetting()
+            {
+                format = GridEditViewBase.columnSetting.formatType.N2,
+                aggregation = GridEditViewBase.columnSetting.aggregationType.SUM,
+                isReadonly = true,
+                order = 9,
+                tooltip = String.Join(
+                     Environment.NewLine,
+                     "Calculation is base on Overhead Type:",
+                     "If 'Normal' then Count*Duration*Rate",
+                     "If 'Hours' then Count*Hours*Rate")
+            });
+            grd.columnCombo.Clear();
+            grd.columnCombo.Add("OverheadGroupID", GridLibrary.CreateCombo("cmbOverheadGroupID", "Overhead Group", vm.cmbOverheadGroupList(), "OverheadGroupID", "Title"));
+            grd.columnCombo.Add("OverheadTypeID", GridLibrary.CreateCombo("cmbOverheadTypeID", "Overhead Type", vm.cmbOverheadTypeList(), "OverheadTypeID", "Title"));
+
+            
+        }
+
+        public override string IssueIfClosed()
+        {
+            bool isvalid = grd.IsValid();
+            if (!isvalid)
+            {
+                return "Not all data in the tab is saved (data in error not saved). Press ok to fix the errors, or press cancel to lose changes in error";
+            }
+            return "";
+        }
+        
+      
+    }
+
+}
---
+++ WalzExplorer/Controls/RHSTabs/Tender/OverheadDetailViewModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data.Entity;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Telerik.Windows.Data;
+using WalzExplorer.Common;
+using WalzExplorer.Controls.Grid;
+using WalzExplorer.Database;
+
+namespace WalzExplorer.Controls.RHSTabs.Tender
+{
+    public class OverheadDetailViewModel :GridEditViewModelBase
+    {
+        int WorkGroupID;
+        int TenderID;
+
+        public OverheadDetailViewModel(WEXSettings settings) //(string NodeType, string PersonID, int Id)
+        {
+            TenderID = ConvertLibrary.StringToInt(settings.node.FindID("TENDER", "-2"), -1);
+            WorkGroupID = ConvertLibrary.StringToInt(settings.node.FindID("TENDER_WORKGROUP", "-2"), -1);
+            data = new ObservableCollection<ModelBase>(context.tblTender_OverheadItem.Where(x => x.WorkGroupID == WorkGroupID).OrderBy(x => x.SortOrder));  
+        }
+
+        public override ModelBase DefaultItem(ModelBase NearItem)
+        {
+            tblTender_OverheadItem i = new tblTender_OverheadItem();
+            i.WorkGroupID = WorkGroupID;
+            return i;
+        }
+        public List<object> cmbOverheadGroupList()
+        {
+            return context.tblTender_OverheadGroup.Where(x => x.TenderID == TenderID).OrderBy(x => x.SortOrder).ToList<object>();
+        }
+
+        public List<object> cmbOverheadTypeList()
+        {
+            return context.tblTender_OverheadType.OrderBy(x => x.SortOrder).ToList<object>();
+        }
+        //public Expression expTotal ()
+        //{
+        //    Expression<Func<tblTender_OverheadItem, double>> expression = x => (double) x.Count*x.Duration*x.Rate;
+
+        //    return expression;
+        //}
+        //public AggregateFunction expTotalAgg()
+        //{
+        //    AggregateFunction<tblTender_OverheadItem, double> aggregate = new AggregateFunction<tblTender_OverheadItem, double>
+        //    {
+        //        AggregationExpression = tblTender_OverheadItem => tblTender_OverheadItem.Sum(x => x.Count * x.Duration * x.Rate),
+        //        Caption = "="
+        //    };
+
+
+        //    return aggregate;
+        //}
+      
+    }
+}
