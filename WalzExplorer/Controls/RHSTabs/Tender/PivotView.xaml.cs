ceaf7ab551a3ceafa571943c0a224cce92447828
pkoay <pkoay@walzconstruction.com>
2015-07-29 11:08:59 +1000

    pivot start

---
+++ WalzExplorer/Controls/RHSTabs/Tender/PivotView.xaml.cs
@@ -0,0 +1,65 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Threading;
+using Telerik.Windows.Controls;
+using Telerik.Windows.Controls.GridView;
+using Telerik.Windows.Data;
+using WalzExplorer.Common;
+using WalzExplorer.Controls.Grid;
+
+namespace WalzExplorer.Controls.RHSTabs.Tender
+{
+    /// <summary>
+    /// Interaction logic for TenderViewer.xaml
+    /// </summary>
+
+
+    public partial class PivotView : RHSTabViewBase
+    {
+
+        //DrawingViewModel vm;
+
+       public PivotView()
+        {
+            InitializeComponent();
+        }
+
+        public override void TabLoad()
+        {
+            //vm = new DrawingViewModel(settings);
+            //grd.vm = vm;
+            //grd.grd.DataContext = vm;
+            //grd.grd.ItemsSource = vm.data;
+
+            //if (settings.user.SecurityGroups.Contains("WD_Tender"))
+            //    grd.SetGrid(settings, true, true, true);
+            //else
+            //    grd.SetGrid(settings, false, false, false);
+
+           
+            //grd.columnsettings.Add("DrawingID", new GridEditViewBase.columnSetting() { isDeveloper = true });
+            //grd.columnsettings.Add("TenderID", new GridEditViewBase.columnSetting() { isDeveloper = true });
+            //grd.columnsettings.Add("Title", new GridEditViewBase.columnSetting() { aggregation = GridEditViewBase.columnSetting.aggregationType.COUNT, format = GridEditViewBase.columnSetting.formatType.TEXT });
+            //grd.columnsettings.Add("Used", new GridEditViewBase.columnSetting() { tooltip="Number of times the Drawing has been selected in the estimate", format = GridEditViewBase.columnSetting.formatType.G, isReadonly=true });
+          
+        }
+
+        public override string IssueIfClosed()
+        {
+            //bool isvalid = grd.IsValid();
+            //if (!isvalid)
+            //{
+            //    return "Not all data in the tab is saved (data in error not saved). Press ok to fix the errors, or press cancel to lose changes in error";
+            //}
+            return "";
+        }
+       
+      
+    }
+
+}
